bd9a42fe26a7620a5efe2fc98adccee1b6b7700c
LiCQing <1464191303@qq.com>
2020-04-10 21:19:27 +0800

    1

--- src/components/container/mainComponent/blogMain/BlogItem.vue
+++ src/components/container/mainComponent/blogMain/BlogItem.vue
@@ -299,7 +299,7 @@
 			},
 			isPlan:function(){
 				var space = (new Date().getTime() - new Date(this.blog.blogSendTime)) / 1000;
-				return space <= 1800 ? true :false;
+				return space < -10 ? true :false;
 			},
 			images: function(){
 				var list = this.blog.blogImgs.split("|").map(img=>{return this.util.VAR().imgurl+img});
